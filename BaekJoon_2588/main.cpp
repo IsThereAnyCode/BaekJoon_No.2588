3a12f485b1b153891ab35139f6242c9678d5caaa
IsThereAnyCode <hjk7625@gmail.com>
2022-09-04 16:23:20 +0900

    Initial Commit

---
+++ BaekJoon_2588/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BaekJoon_2588
+//
+//  Created by NoMore on 2022/09/04.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
